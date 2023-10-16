f1b3ae50ed5e2b94bdb04489fff22f0f945ba05c
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2023-10-16 14:43:21 -0400

    Some tests

--- lab4.java
+++ lab4.java
@@ -1,5 +1,17 @@
 public class lab4 {
     public static void main(String args[]){
-        
+        BTPrinting btPrinting = new BTPrinting();
+        System.out.println("Aidan - BTPrinting:");
+        btPrinting.testMethods();
+        System.out.println();
+
+        searchBT searchBT = new searchBT();
+        System.out.println("Ali - SearchBT:");
+        searchBT.testMethods();
+        System.out.println();
+
+        aggregateBT aggregateBT = new aggregateBT();
+        System.out.println("Nikan - AggregateBT:");
+        aggregateBT.testMethods();
     }
 }
