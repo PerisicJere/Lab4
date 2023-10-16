8390185bcf47e6005f596146f52ebb8cc1789663
Nikan <nikankad@protonmail.com>
2023-10-16 14:10:10 -0400

    Update aggregateBT.java

--- aggregateBT.java
+++ aggregateBT.java
@@ -1,10 +1,17 @@
 public class aggregateBT extends BinaryTree {
+
     public int countEvenNumber(){
         int evenCount = 0;
 
         return evenCount;
     }
 
+
+    public int findSum(){
+        int sum = 0;
+        
+        return sum;
+    }
 
 
 
