9dd5ad5c832d006f222c0d6add14998e376ae1fa
Nikan <nikankad@protonmail.com>
2023-10-16 14:43:17 -0400

    added comments to binary tree

--- aggregateBT.java
+++ aggregateBT.java
@@ -3,96 +3,96 @@
 // AggregateBT class extends BinaryTree and contains methods to work with binary trees
 public class aggregateBT extends BinaryTree {
 
-    // Constructor for AggregateBT class, initializing the binary tree
-    public aggregateBT() {
-        super();
+  // Constructor for AggregateBT class, initializing the binary tree
+  public aggregateBT() {
+    super();
+  }
+
+  /**
+   * Recursively counts the number of nodes with even values in the binary tree.
+   *
+   * @param node The current node being processed in the recursion.
+   * @return The number of nodes with even values in the subtree rooted at the given node.
+   */
+  public int countEvenNumberRecursive(Node node) {
+    // Base case: if the node is null, there are no even nodes, return 0
+    if (node == null) {
+      return 0;
     }
 
-    /**
-     * Recursively counts the number of nodes with even values in the binary tree.
-     *
-     * @param node The current node being processed in the recursion.
-     * @return The number of nodes with even values in the subtree rooted at the given node.
-     */
-    public int countEvenNumberRecursive(Node node) {
-        // Base case: if the node is null, there are no even nodes, return 0
-        if (node == null) {
-            return 0;
-        }
+    // Initialize a counter for even nodes
+    int evenCount = 0;
 
-        // Initialize a counter for even nodes
-        int evenCount = 0;
-
-        // Check if the current node's value is even, increment the counter if true
-        if (node.data % 2 == 0) {
-            evenCount++;
-        }
-
-        // Recursively count even nodes in the left and right subtrees and accumulate the count
-        evenCount = evenCount + countEvenNumberRecursive(node.left);
-        evenCount = evenCount + countEvenNumberRecursive(node.right);
-
-        // Return the total count of even nodes
-        return evenCount;
+    // Check if the current node's value is even, increment the counter if true
+    if (node.data % 2 == 0) {
+      evenCount++;
     }
 
-    /**
-     * Recursively finds the summation of values in the binary tree.
-     *
-     * @param node The current node being processed in the recursion.
-     * @return The sum of values in the subtree rooted at the given node.
-     */
-    public int findSumRecursive(Node node) {
-        // Base case: if the node is null, the sum is 0
-        if (node == null) {
-            return 0;
-        }
+    // Recursively count even nodes in the left and right subtrees and accumulate the count
+    evenCount = evenCount + countEvenNumberRecursive(node.left);
+    evenCount = evenCount + countEvenNumberRecursive(node.right);
 
-        // Initialize a variable to store the sum of node values
-        int sum = node.data;
+    // Return the total count of even nodes
+    return evenCount;
+  }
 
-        // Recursively find the sum of values in the left and right subtrees and accumulate the sum
-        sum += findSumRecursive(node.left);
-        sum += findSumRecursive(node.right);
-
-        // Return the total sum of node values
-        return sum;
+  /**
+   * Recursively finds the summation of values in the binary tree.
+   *
+   * @param node The current node being processed in the recursion.
+   * @return The sum of values in the subtree rooted at the given node.
+   */
+  public int findSumRecursive(Node node) {
+    // Base case: if the node is null, the sum is 0
+    if (node == null) {
+      return 0;
     }
 
-    /**
-     * Public method to initiate the recursive process of finding the sum of values in the tree.
-     *
-     * @return The sum of all values in the binary tree.
-     */
-    public int findSum() {
-        return findSumRecursive(root); // Start recursion from the root node
-    }
+    // Initialize a variable to store the sum of node values
+    int sum = node.data;
 
-    /**
-     * Public method to initiate the recursive process of counting nodes with even values.
-     *
-     * @return The number of nodes with even values in the binary tree.
-     */
-    public int findEvenSum() {
-        return countEvenNumberRecursive(root); // Start recursion from the root node
-    }
+    // Recursively find the sum of values in the left and right subtrees and accumulate the sum
+    sum += findSumRecursive(node.left);
+    sum += findSumRecursive(node.right);
 
-    @Override
-    public  void testMethods() {
-      aggregateBT tree = new aggregateBT();
-      // Creating a sample tree
-      tree.root = new Node(1);
-      tree.root.left = new Node(2);
-      tree.root.right = new Node(3);
-      tree.root.left.left = new Node(4);
-      tree.root.left.right = new Node(5);
+    // Return the total sum of node values
+    return sum;
+  }
 
-      // Calling and printing the output of countEvenNumber() method
-      int evenCount = tree.findEvenSum();
-      System.out.println("Number of nodes with even values: " + evenCount);
+  /**
+   * Public method to initiate the recursive process of finding the sum of values in the tree.
+   *
+   * @return The sum of all values in the binary tree.
+   */
+  public int findSum() {
+    return findSumRecursive(root); // Start recursion from the root node
+  }
 
-      // Calling and printing the output of findSum() method
-      int sum = tree.findSum();
-      System.out.println("Sum of values in the tree: " + sum);
+  /**
+   * Public method to initiate the recursive process of counting nodes with even values.
+   *
+   * @return The number of nodes with even values in the binary tree.
+   */
+  public int findEvenSum() {
+    return countEvenNumberRecursive(root); // Start recursion from the root node
+  }
+
+  @Override
+  public void testMethods() {
+    aggregateBT tree = new aggregateBT();
+    // Creating a sample tree
+    tree.root = new Node(1);
+    tree.root.left = new Node(2);
+    tree.root.right = new Node(3);
+    tree.root.left.left = new Node(4);
+    tree.root.left.right = new Node(5);
+
+    // Calling and printing the output of countEvenNumber() method
+    int evenCount = tree.findEvenSum();
+    System.out.println("Number of nodes with even values: " + evenCount);
+
+    // Calling and printing the output of findSum() method
+    int sum = tree.findSum();
+    System.out.println("Sum of values in the tree: " + sum);
   }
 }
