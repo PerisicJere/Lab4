3a9fd7135cfa3adb115a03e7cae789fe66e2e039
PerisicJere <117933956+PerisicJere@users.noreply.github.com>
2023-10-16 14:47:59 -0400

    Some tests

--- lab4.java
+++ lab4.java
@@ -1,15 +1,28 @@
+/**
+ * @author Jere Perisic
+ * 
+ */
 public class lab4 {
+
     public static void main(String args[]){
+        /**
+         * Testing Aidans BTPrinting method
+         */
         BTPrinting btPrinting = new BTPrinting();
         System.out.println("Aidan - BTPrinting:");
         btPrinting.testMethods();
         System.out.println();
-
+        /**
+         * Testing Ali's searchBT method
+         */
         searchBT searchBT = new searchBT();
         System.out.println("Ali - SearchBT:");
         searchBT.testMethods();
         System.out.println();
-
+        /**
+         * Testing Nikan's aggregateBT method
+         *
+         */
         aggregateBT aggregateBT = new aggregateBT();
         System.out.println("Nikan - AggregateBT:");
         aggregateBT.testMethods();
