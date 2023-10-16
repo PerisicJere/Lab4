446f9c76dd9838d1a1e204711626a4d2ec523b05
Jerry <117933956+PerisicJere@users.noreply.github.com>
2023-10-16 14:10:00 -0400

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,11 +1,25 @@
 public class BinaryTree {
-    protected Node root;
+    private Node root;
+
+    private static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
 
     public BinaryTree() {
         root = null;
     }
 
+    public void insert(int value) {
+        root = insertRec(root, value);
+    }
     public void testing() {
     }
-
 }
