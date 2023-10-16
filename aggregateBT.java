952480f15289c16437df9837429eb3829850b0ec
Nikan <nikankad@protonmail.com>
2023-10-16 14:29:02 -0400

    added more comments

--- aggregateBT.java
+++ aggregateBT.java
@@ -3,19 +3,24 @@
 // AggregateBT class extends BinaryTree and contains methods to work with binary trees
 public class aggregateBT extends BinaryTree {
 
-    // Constructor for AggregateBT class, calling the constructor of the superclass (BinaryTree)
+    // Constructor for AggregateBT class, initializing the binary tree
     public aggregateBT() {
         super();
     }
 
-    // Recursive method to count nodes with even values
+    /**
+     * Recursively counts the number of nodes with even values in the binary tree.
+     *
+     * @param node The current node being processed in the recursion.
+     * @return The number of nodes with even values in the subtree rooted at the given node.
+     */
     public int countEvenNumberRecursive(Node node) {
-        // Base case: if the node is null, return 0
+        // Base case: if the node is null, there are no even nodes, return 0
         if (node == null) {
             return 0;
         }
 
-        // Initialize a counter for even numbers
+        // Initialize a counter for even nodes
         int evenCount = 0;
 
         // Check if the current node's value is even, increment the counter if true
@@ -31,9 +36,14 @@ public class aggregateBT extends BinaryTree {
         return evenCount;
     }
 
-    // Recursive method to find the summation of values in the tree
+    /**
+     * Recursively finds the summation of values in the binary tree.
+     *
+     * @param node The current node being processed in the recursion.
+     * @return The sum of values in the subtree rooted at the given node.
+     */
     public int findSumRecursive(Node node) {
-        // Base case: if the node is null, return 0
+        // Base case: if the node is null, the sum is 0
         if (node == null) {
             return 0;
         }
@@ -49,12 +59,20 @@ public class aggregateBT extends BinaryTree {
         return sum;
     }
 
-    // Public method to initiate the recursive process of finding the sum of values in the tree
+    /**
+     * Public method to initiate the recursive process of finding the sum of values in the tree.
+     *
+     * @return The sum of all values in the binary tree.
+     */
     public int findSum() {
         return findSumRecursive(root); // Start recursion from the root node
     }
 
-    // Public method to initiate the recursive process of counting nodes with even values
+    /**
+     * Public method to initiate the recursive process of counting nodes with even values.
+     *
+     * @return The number of nodes with even values in the binary tree.
+     */
     public int findEvenSum() {
         return countEvenNumberRecursive(root); // Start recursion from the root node
     }
